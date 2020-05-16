0591c7d7e0b772561f13e0f80ce854838d1ae1da
Mamta Bhardwaj <mamta.bhardwaj@builder.ai>
2020-05-16 19:39:56 +0530

    Add Organization route and controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,39 @@
+class OrganizationsController < ApplicationController
+  before_action :set_organization, only: [:update, :destroy]
+
+  def create
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      render json: @organization.as_json
+    else
+        render json: @organization.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @organization.update(organization_params)
+      render json: @organization.as_json
+    else
+      render json: @organization.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @organization.destroy
+      render json: { message: 'Organization deleted successfully!!' }
+    else
+      render json: @organization.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_organization
+      @organization = Organization.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def organization_params
+      params.permit(:id, :name)
+    end
+end
